7ea1b14e611a485bf316cbe1799b362e6f2513a4
MealTeamSixNZ <120446542+MealTeamSixNZ@users.noreply.github.com>
2024-08-08 08:08:24 +1200

    add validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+
+  validates :player, length: {minimum: 2, maximum: 24 }, format: {with: /\A[ a-zA-Z]+\z/}, presence: true
+  validates :lives, numericality: { only_integer: true }, inclusion: 0..13, presence: true
 end
